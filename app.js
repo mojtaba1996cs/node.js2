f30d62ded5ecbd72e3365c76815353338dc5df74
mojtaba1996php <48107577+mojtaba1996cs@users.noreply.github.com>
2021-09-12 02:56:03 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-const app = require("express")();
-
+const express = require('express');
+const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded());
 
-app.get("", (req, res) => {
-  res.send("Hello world");
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.listen(PORT, () => {
