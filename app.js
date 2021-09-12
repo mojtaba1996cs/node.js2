c481c9193c5c7644393ccb2b5b9d87635bcdebac
mojtaba1996php <48107577+mojtaba1996cs@users.noreply.github.com>
2021-09-12 03:01:09 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const urlShortener = require('node-url-shortener');
 const PORT = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -11,8 +12,10 @@ app.get('/', function(req, res) {
 });
 app.post('/url', function(req, res) {
     const url = req.body.url;
-
-    res.send(url);
+     urlShortener.short(url, function(err, shortUrl){
+        res.send(shortUrl);
+    });
+   
 });
 
 app.listen(PORT, () => {
